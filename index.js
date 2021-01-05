54bf02bb00674e638110780ac772dff748e589a1
sabbir-320 <sabbir195661@gmail.com>
2021-01-06 00:16:17 +0600

    code organaiz

--- index.js
+++ index.js
@@ -36,72 +36,59 @@ client.connect(err => {
     const file = req.files.file
     const title = req.body.title
 
-    console.log(req.body);
-    const filePath = `${__dirname}/volunteers/${file.name}`
-    file.mv(filePath, err => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send({ msg: 'file upload failed' })
-      }
-      const newImg = fs.readFileSync(filePath)
-      const encImg = newImg.toString('base64')
-      const image = {
-        contentType: req.files.file.mimetype,
-        size: req.files.file.size,
-        img: Buffer(encImg, 'base64')
-      }
+    const newImg = file.data
+    const encImg = newImg.toString('base64')
+    const image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    }
 
-      collection.insertOne({ title, image })
-        .then(result => {
-          fs.remove(filePath, error => {
-            if (error) {
-              console.log(error);
-              res.status(500).send({ msg: 'file upload failed' })
-            }
-            res.send(result.insertedCount > 0)
-          })
-        })
+    collection.insertOne({ title, image })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+})
+
+// get grud 
+app.get('/volunteerItems', (req, res) => {
+  collection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
     })
-  })
+})
 
-  // get grud 
-  app.get('/volunteerItems', (req, res) => {
-    collection.find({})
-      .toArray((err, documents) => {
-        res.send(documents);
-      })
+app.get('/register/:id', (req, res) => {
+  collection.find({
+    _id: ObjectId(`${req.params.id}`)
   })
-
-  app.get('/register/:id', (req, res) => {
-    collection.find({
-      _id: ObjectId(`${req.params.id}`)
+    .toArray((err, document) => {
+      res.send(document)
+      console.log(err);
     })
-      .toArray((err, document) => {
-        res.send(document)
-        console.log(err);
-      })
-  })
+})
 
-  // perticuler user item
-  app.get('/registerdItem/:_id', (req, res) => {
-    console.log(req.params.id);
-    collection.find({
-      _id: ObjectId(`${req.params.id}`)
+// perticuler user item
+app.get('/registerdItem/:_id', (req, res) => {
+  console.log(req.params.id);
+  collection.find({
+    _id: ObjectId(`${req.params.id}`)
+  })
+    .toArray((err, document) => {
+      console.log(document);
+      res.send(document)
+      console.log(err);
     })
-      .toArray((err, document) => {
-        console.log(document);
-        res.send(document)
-        console.log(err);
-      })
-  })
+})
 
-  // app grud by email
-  app.get('/volunteerByEmail', (req, res) => {
-    collection.find({ email: req.query.email })
-      .toArray((err, document) => {
-        res.send(document)
-      })
-  })
+// app grud by email
+app.get('/volunteerByEmail', (req, res) => {
+  collection.find({ email: req.query.email })
+    .toArray((err, document) => {
+      res.send(document)
+    })
+})
 
 
 
